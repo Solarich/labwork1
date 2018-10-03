e3a32aea643ce41ee4376720cdde8e59e0a77d1e
R4NL <Alexfenin1234@gmail.com>
2018-10-03 17:22:27 +0300

    Dao

--- .idea/src/Servise/ChatServer.java
+++ .idea/src/Servise/ChatServer.java
@@ -1,11 +1,53 @@
 package Servise;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ChatServer {
     private ServerSocket serverSocket;
     private List<SingleConnection> connectionList= new ArrayList<>();
+    private List<Message> messageList= Collections.synchronizedList(new ArrayList<>());
 
+    public ChatServer() {
+    }
+
+    private boolean init() {
+        try {
+            this.serverSocket = new ServerSocket(20000);
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            return false;
+        }
+        return true;
+    }
+    public void serverStart() {
+        boolean startInit = this.init();
+        if (startInit == false) {
+            System.out.println("Server initialization failed ....");
+            return;
+        }
+        System.out.println("Server start");
+        int i = 0;
+        for (;;) {
+            try {
+                Socket socket = this.serverSocket.accept();
+                MessageProviderImpl mpi = new MessageProviderImpl();
+                mpi.setSocket(socket);
+                SingleConnection singleConnection = new SingleConnection(mpi, connectionList, messageList, i++);
+                Thread threadSingleConnection = new Thread(singleConnection);
+                threadSingleConnection.setDaemon(true);
+                threadSingleConnection.start();
+
+            } catch (IOException e) {
+                System.out.println("Server stop");
+                return;
+            }
+
+        }
+    }
 }
---
+++ .idea/src/Servise/Main.java
@@ -0,0 +1,10 @@
+package Servise;
+
+public class Main {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        ChatServer chatServer = new ChatServer();
+        chatServer.serverStart();
+    }
+
+}
