3ed7b66688db14cbb1ebc2134e8db5807eba40c6
R4NL <Alexfenin1234@gmail.com>
2018-10-03 17:34:05 +0300

    Dao

--- .idea/src/Server/Servise/MessageRenderImpl.java
+++ .idea/src/Server/Servise/MessageRenderImpl.java
@@ -4,13 +4,13 @@ import java.text.SimpleDateFormat;
 
 public class MessageRenderImpl implements MessageRander {
     @Override
-    public String massegeRander(Message message) {
+    public void massegeRander(Message message) {
         if (message==null){
             throw new RuntimeException("Get null message");
         }
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMMM yyyy - hh:mm");
         String text=message.getSender()+" "+simpleDateFormat.format(message.getDepartmentTime())+">"+message.getText()
                 +System.lineSeparator();
-        return text;
+        System.out.println(text);
     }
 }
