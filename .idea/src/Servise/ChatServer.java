6a7c01b858516aa7ea2f91ce5fd2da08667c3c7b
R4NL <Alexfenin1234@gmail.com>
2018-10-03 17:10:00 +0300

    Dao

---
+++ .idea/src/Servise/ChatServer.java
@@ -0,0 +1,11 @@
+package Servise;
+
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServer {
+    private ServerSocket serverSocket;
+    private List<SingleConnection> connectionList= new ArrayList<>();
+
+}
